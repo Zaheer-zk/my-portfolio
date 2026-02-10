6b708febd1f6c6012a41bb104f21ac2330627e97
Zaheer-zk <newzk16117541@gmail.com>
2026-02-10 11:45:28 +0530

    feat: Implement a new falling animation for shooting stars with updated initial positions and animation timings.

--- src/components/ui/shooting-stars.tsx
+++ src/components/ui/shooting-stars.tsx
@@ -13,10 +13,10 @@ export const ShootingStars = ({
 
   React.useEffect(() => {
     const styles = new Array(number || 20).fill(true).map(() => ({
-      top: 0,
-      left: Math.floor(Math.random() * (window.innerWidth) - window.innerWidth / 2) + "px",
-      animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
-      animationDuration: Math.floor(Math.random() * (10 - 2) + 2) + "s",
+      top: -5,
+      left: Math.floor(Math.random() * window.innerWidth) + "px",
+      animationDelay: Math.random() * 1 + 0.2 + "s",
+      animationDuration: Math.floor(Math.random() * 8 + 2) + "s",
     }));
     setMeteorStyles(styles);
   }, [number]);
@@ -27,7 +27,7 @@ export const ShootingStars = ({
         <span
           key={"meteor" + idx}
           className={cn(
-            "animate-meteor absolute top-1/2 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[215deg]",
+            "animate-meteor-falling absolute top-0 left-1/2 h-0.5 w-0.5 rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10] rotate-[90deg]",
             "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
             className
           )}
